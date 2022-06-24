bdedeccd2b170507f6722038e48815522da3704a
shamrasal <shameshrasal77@gmail.com>
2022-06-24 19:03:02 +0530

    added card and arrow function

---
+++ App.js
@@ -0,0 +1,14 @@
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  return (
+    <div>
+      <h1>lets get started</h1>
+      <div>
+        <Expenses></Expenses>
+      </div>
+    </div>
+  )
+
+}
+export default App;
---
+++ components/Expenses/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css"
+import Card from "../UI/Card";
+
+const ExpenseItem = (props) => {
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <ExpenseDetails Title={props.Title} location={props.location} amount={props.amount}></ExpenseDetails>
+        </Card>
+    );
+}
+export default ExpenseItem;
+
+

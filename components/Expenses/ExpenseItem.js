bf3974e1e14ab9f68048f1b58d7a0c2f4ebf8f9b
shamrasal <shameshrasal77@gmail.com>
2022-06-26 12:56:32 +0530

    added delete and change title button

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -4,10 +4,19 @@ import "./ExpenseItem.css"
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log("updated")
+    }
+    const deleteHandler = () => {
+        console.log("deleted")
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <ExpenseDetails Title={props.Title} location={props.location} amount={props.amount}></ExpenseDetails>
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteHandler}>Delete Item</button>
         </Card>
     );
 }

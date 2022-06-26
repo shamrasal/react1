a5fbb0eda6600f7765c8125228cd9227e1d63102
shamrasal <shameshrasal77@gmail.com>
2022-06-26 15:45:03 +0530

    usestate and setstate tutorial

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,21 +1,31 @@
+import React, { useState } from "react";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.Title);
+    const [amount, setAmount] = useState(props.amount);
+
     const clickHandler = () => {
-        console.log("updated")
+        setTitle('updated')
+        console.log(title)
+    }
+    const editAmountHandler = () => {
+        setAmount(100)
+        console.log(amount)
     }
     const deleteHandler = () => {
-        console.log("deleted")
+        console.log('deleted')
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
-            <ExpenseDetails Title={props.Title} location={props.location} amount={props.amount}></ExpenseDetails>
+            <ExpenseDetails Title={title} location={props.location} amount={amount}></ExpenseDetails>
             <button onClick={clickHandler}>Change Title</button>
+            <button onClick={editAmountHandler}>Change Amount</button>
             <button onClick={deleteHandler}>Delete Item</button>
         </Card>
     );

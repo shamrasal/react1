753963d3faf259acfa6051afddb7d185d1206f32
shamrasal <shameshrasal77@gmail.com>
2022-06-28 18:57:18 +0530

    filter methd added in expense list

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -1,12 +1,26 @@
-
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem"
 import "./Expenses.css"
 const Expenses = (props) => {
+    const [filteredyear, setfilteredYear] = useState('2020');
+
+    const onChangeFilterHandler = (selectedYear) => {
+        setfilteredYear(selectedYear)
+    }
+
+    const filteredexpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredyear
+    })
     return (
         <div>
             <Card className="expenses">
-                {props.items.map((expense) => (
+                <ExpensesFilter
+                    selected={filteredyear}
+                    onChangeFilter={onChangeFilterHandler}
+                />
+                {filteredexpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         Title={expense.title}

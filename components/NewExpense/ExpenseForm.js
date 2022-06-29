7b48ff37c6b56e352dc31f83c60ad170393f717f
shamrasal <shameshrasal77@gmail.com>
2022-06-29 12:19:55 +0530

    new Expense button added for conditional return

--- components/NewExpense/ExpenseForm.js
+++ components/NewExpense/ExpenseForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-
 import './ExpenseForm.css'
 
 const ExpenseForm = (props) => {
@@ -78,6 +77,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='button' onClick={props.onCancel}>cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
--- components/NewExpense/NewExpense.js
+++ components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react'
 import './NewExpense.css'
 
 import ExpenseForm from './ExpenseForm.js'
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false)
 
     const onAddExpenseHandler = (enteredExpense) => {
         const expenseData = {
@@ -10,12 +12,22 @@ const NewExpense = (props) => {
             id: Math.random().toString
         }
         props.onGetExpenseInApp(expenseData)
+        setIsEditing(false)
+    }
+
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+    
+    const cancelClickHandler = () => {
+        setIsEditing(false)
     }
 
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseForm={onAddExpenseHandler}></ExpenseForm>
+            {!isEditing && <button onClick={startEditingHandler}>Add new Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseForm={onAddExpenseHandler} onCancel={cancelClickHandler}></ExpenseForm>}
         </div>
     )
 }
